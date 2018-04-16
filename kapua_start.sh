5f610d618123db162be74f9fbbeca8d2fed70338
root <juagilest@gmail.com>
2018-04-16 22:15:04 +0200

    HulFlyS

---
+++ kapua_start.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+docker start kapua-sql
+docker start kapua-elasticsearch  
+docker start kapua-broker
+docker start kapua-api
+docker start kapua-console
---
+++ kapua_stop.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+docker stop kapua-sql
+docker stop kapua-console
+docker stop kapua-api
+docker stop kapua-elasticsearch 
+docker stop kapua-broker 
